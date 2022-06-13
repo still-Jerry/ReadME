3f593b73fdda1686b3d41a21010f46c8c52b281e
still_Jerry <oljaapelsin@gmail.com>
2022-06-13 12:01:23 +0300

    Initial Commit

---
+++ ReadME/ReadMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMEApp.swift
+//  ReadME
+//
+//  Created by Anatolich Mixaill on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
